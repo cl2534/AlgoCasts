924de732df4e175baeb5e32e1723ae45e3ceec36
github username <chang.lim92@gmail.com>
2018-09-25 18:18:16 -0400

    chunk and fizzbuzz

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,7 +7,30 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+//
+// function chunk(array, size) {
+//   let chunk = []
+//   for (let element of array) {
+//     let lastEle = chunk[chunk.length-1];
+//     if (!lastEle || lastEle.length === size) {
+//       chunk.push([element])
+//     } else {
+//       lastEle.push(element)
+//     }
+//   }
+// return chunk
+// }
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = []
+  let index = 0
+  while (index < array.length) {
+    chunked.push(array.slice(index, index+size))
+    // index would be the start point & can be where it is left over from first loop
+    // if the array is [1,2,3,4,5] and array.slice(3,6+) will always return index 3 to the end.
+    index += size
+  }
+  return chunked
+}
 
 module.exports = chunk;
