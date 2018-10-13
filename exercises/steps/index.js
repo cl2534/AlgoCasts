b449ca0011917f68765ac19a7e2dc56d8c1e5aa7
github username <chang.lim92@gmail.com>
2018-10-13 16:14:50 -0400

    Steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,35 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
+
+//Alternate Solution for using Recursive method.
+const steps = (n, row = 0, stair = '') => {
+    if (n === row) {
+      return
+    }
+    if (stair.length === n) {
+      console.log(stair)
+      return steps(n, row+1)
+    }
+    if (stair.length <= row) {
+      stair += '#'
+    } else {
+      stair += ' '
+    }
+    return steps(n, row, stair)
+}
 
 module.exports = steps;
