27ca5551543797164c9109d8569784ac55ff9881
github username <chang.lim92@gmail.com>
2018-10-25 15:46:48 -0400

    10/25 pyramid, steps, vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,37 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   const midpoint = Math.floor((2*n-1)/2)
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+//     for (let column = 0; column < 2*n -1; column++) {
+//       if (midpoint - row <= column & midpoint + row >= column) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
 
+
+function pyramid(n, row = 0, stair = '') {
+  if (row === n) {
+    return;
+  }
+  if (stair.length === 2* n-1) {
+    console.log(stair)
+    return pyramid(n, row+1)
+  }
+  const midpoint = Math.floor((2*n-1)/2)
+  let add;
+  if (midpoint - row <= stair.length && midpoint + row >= stair.length) {
+    add = '#'
+  } else {
+    add = ' '
+  }
+  pyramid(n, row, stair+ add)
+}
 module.exports = pyramid;
