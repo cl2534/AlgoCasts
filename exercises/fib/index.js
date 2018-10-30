ed2d0744d474c4f6940158b0b4f7f3647ba68afb
github username <chang.lim92@gmail.com>
2018-10-30 09:42:00 -0400

    fib / matrix / queue

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,11 +9,19 @@
 //   fib(4) === 3
 
 function fib(n) {
-  if (n < 2) {
-    return n
-  } else {
-    return fib(n-1) + fib(n-2)
+  // if (n < 2) {
+  //   return n
+  // } else {
+  //   return fib(n-1) + fib(n-2)
+  // }
+  const result = [0 , 1]
+  for (let i = 2; i <= n; i++) {
+    const a = result[i-1]
+    const b = result[i-2]
+    result.push(a+b)
   }
+  return result[result.length-1]
+
 }
 
 module.exports = fib;
